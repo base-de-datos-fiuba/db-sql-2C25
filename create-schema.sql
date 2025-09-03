60c308879e930c931bc89ca65efa306cad3042e6
frandelu <franciscodelucaa@gmail.com>
2025-09-03 13:03:25 -0300

    Re-Factor .sql file

--- create-schema.sql
+++ create-schema.sql
@@ -10,9 +10,9 @@ CREATE TABLE pokemon (
   spattack SMALLINT NOT NULL,
   spdefense SMALLINT NOT NULL,
   speed SMALLINT NOT NULL,
+  dualtype BOOLEAN NOT NULL DEFAULT false,
   PRIMARY KEY (IDpoke)
 );
-
 
 CREATE TABLE egggroup (
   IDegg SMALLINT NOT NULL ,
@@ -33,10 +33,6 @@ CREATE TABLE abilities (
 	PRIMARY KEY (IDability)
 );
 
-
-
-
-
 CREATE TABLE "poke-type" (
 	IDpoke SMALLINT NOT NULL ,
 	IDtype SMALLINT NOT NULL,
@@ -45,7 +41,6 @@ CREATE TABLE "poke-type" (
 	CONSTRAINT fk_poketype_type FOREIGN KEY (IDtype) REFERENCES type (IDtype)
 );
 
-
 CREATE TABLE "poke-egg" (
 	IDpoke SMALLINT NOT NULL ,
 	IDegg SMALLINT NOT NULL,
@@ -53,7 +48,6 @@ CREATE TABLE "poke-egg" (
 	CONSTRAINT fk_pokeegg_poke FOREIGN KEY (IDpoke) REFERENCES pokemon (IDpoke),
 	CONSTRAINT fk_pokeegg_egg FOREIGN KEY (IDegg) REFERENCES egggroup (IDegg)
 );
-
 
 CREATE TABLE "poke-ability" (
 	IDpoke SMALLINT NOT NULL ,
@@ -64,13 +58,54 @@ CREATE TABLE "poke-ability" (
 	CONSTRAINT fk_pokeabi_abi FOREIGN KEY (IDability) REFERENCES abilities (IDability)
 );
 
+CREATE TABLE categories (
+  IDcat SMALLINT NOT NULL,
+  category CHAR(8) NOT NULL,
+   PRIMARY KEY (IDcat)
+);
 
+CREATE TABLE moves (
+  IDmove SMALLINT NOT NULL,
+  movename CHAR(16) NOT NULL ,
+  IDtype SMALLINT NOT NULL,
+  IDcat SMALLINT NOT NULL,
+  power SMALLINT NOT NULL,
+  accuracy SMALLINT NOT NULL,
+  PP SMALLINT NOT NULL,
+  effect VARCHAR(255) NOT NULL,
+  PRIMARY KEY (IDmove),
+  CONSTRAINT fk_moves_type FOREIGN KEY (IDtype) REFERENCES type (IDtype),
+  CONSTRAINT fk_moves_cat FOREIGN KEY (IDcat) REFERENCES categories (IDcat)
+);
 
+CREATE TABLE learnsets (
+	IDpoke SMALLINT NOT NULL,
+	IDmove SMALLINT NOT NULL,
+    level SMALLINT NOT NULL,
+	PRIMARY KEY (IDpoke,IDmove),
+	CONSTRAINT fk_learnset_poke FOREIGN KEY (IDpoke) REFERENCES pokemon (IDpoke),
+	CONSTRAINT fk_learnset_move FOREIGN KEY (IDmove) REFERENCES moves (IDmove)
+);
 
 INSERT INTO pokemon VALUES (3, 'Venusaur', 80, 82, 83, 100, 100, 80);
 INSERT INTO pokemon VALUES (6, 'Charizard', 78, 84, 78, 109, 85, 100);
 INSERT INTO pokemon VALUES (9, 'Blastoise', 79, 83, 100, 85, 105, 78);
+INSERT INTO pokemon VALUES (12, 'Butterfree', 60, 45, 50, 90, 80, 70, true);
+INSERT INTO pokemon VALUES (15, 'Beedrill', 65, 90, 40, 45, 80, 75, true);
+INSERT INTO pokemon VALUES (18, 'Pidgeot', 83, 80, 75, 70, 70, 101, true);
+INSERT INTO pokemon VALUES (26, 'Raichu', 60, 90, 55, 90, 80, 110, false);
+INSERT INTO pokemon VALUES (28, 'Sandslash', 75, 100, 110, 45, 55, 65, false);
+INSERT INTO pokemon VALUES (36, 'Clefable', 95, 70, 73, 95, 90, 60, false);
+INSERT INTO pokemon VALUES (62, 'Poliwrath', 90, 95, 95, 70, 90, 70, true);
+INSERT INTO pokemon VALUES (94, 'Gengar', 60, 65, 60, 130, 75, 110, true);
+INSERT INTO pokemon VALUES (124, 'Jynx', 65, 50, 35, 115, 95, 95, true);
+INSERT INTO pokemon VALUES (139, 'Omastar', 70, 60, 125, 115, 70, 55, true);
+INSERT INTO pokemon VALUES (376, 'Metagross', 80, 135, 130, 95, 90, 70, true);
+INSERT INTO pokemon VALUES (635, 'Hydreigon', 92, 105, 90, 125, 90, 98, true);
 
+UPDATE pokemon SET dualtype=true WHERE IDpoke=3;
+UPDATE pokemon SET dualtype=true WHERE IDpoke=6;
+UPDATE pokemon SET dualtype=false WHERE IDpoke=9;
 
 INSERT INTO type VALUES (1, 'Normal');
 INSERT INTO type VALUES (2, 'Fighting');
@@ -91,31 +126,11 @@ INSERT INTO type VALUES (16, 'Dragon');
 INSERT INTO type VALUES (17, 'Dark');
 INSERT INTO type VALUES (18, 'Fairy');
 
-
-ALTER TABLE pokemon ADD COLUMN dualtype BOOLEAN;
-UPDATE pokemon SET dualtype=true WHERE IDpoke=3;
-UPDATE pokemon SET dualtype=true WHERE IDpoke=6;
-UPDATE pokemon SET dualtype=false WHERE IDpoke=9;
-ALTER TABLE pokemon ALTER COLUMN dualtype SET NOT NULL;
 INSERT INTO "poke-type" VALUES (3,12);
 INSERT INTO "poke-type" VALUES (3,4);
 INSERT INTO "poke-type" VALUES (6,10);
 INSERT INTO "poke-type" VALUES (6,3);
 INSERT INTO "poke-type" VALUES (9,11);
-
-
-INSERT INTO pokemon VALUES (12, 'Butterfree', 60, 45, 50, 90, 80, 70, true);
-INSERT INTO pokemon VALUES (15, 'Beedrill', 65, 90, 40, 45, 80, 75, true);
-INSERT INTO pokemon VALUES (18, 'Pidgeot', 83, 80, 75, 70, 70, 101, true);
-INSERT INTO pokemon VALUES (26, 'Raichu', 60, 90, 55, 90, 80, 110, false);
-INSERT INTO pokemon VALUES (28, 'Sandslash', 75, 100, 110, 45, 55, 65, false);
-INSERT INTO pokemon VALUES (36, 'Clefable', 95, 70, 73, 95, 90, 60, false);
-INSERT INTO pokemon VALUES (62, 'Poliwrath', 90, 95, 95, 70, 90, 70, true);
-INSERT INTO pokemon VALUES (94, 'Gengar', 60, 65, 60, 130, 75, 110, true);
-INSERT INTO pokemon VALUES (124, 'Jynx', 65, 50, 35, 115, 95, 95, true);
-INSERT INTO pokemon VALUES (139, 'Omastar', 70, 60, 125, 115, 70, 55, true);
-INSERT INTO pokemon VALUES (376, 'Metagross', 80, 135, 130, 95, 90, 70, true);
-INSERT INTO pokemon VALUES (635, 'Hydreigon', 92, 105, 90, 125, 90, 98, true);
 INSERT INTO "poke-type" VALUES (12,7);
 INSERT INTO "poke-type" VALUES (12,3);
 INSERT INTO "poke-type" VALUES (15,7);
@@ -138,8 +153,6 @@ INSERT INTO "poke-type" VALUES (376,14);
 INSERT INTO "poke-type" VALUES (635,16);
 INSERT INTO "poke-type" VALUES (635,17);
 
-
-
 INSERT INTO egggroup VALUES (1, 'Monster');
 INSERT INTO egggroup VALUES (2, 'Water 1');
 INSERT INTO egggroup VALUES (3, 'Bug');
@@ -156,7 +169,6 @@ INSERT INTO egggroup VALUES (13, 'Ditto');
 INSERT INTO egggroup VALUES (14, 'Dragon');
 INSERT INTO egggroup VALUES (15, 'Undiscovered');
 INSERT INTO egggroup VALUES (16, 'Genderless');
-
 
 INSERT INTO abilities VALUES (65, 'Overgrow', 'Amplifies Grass-type attacks when HP is low');
 INSERT INTO abilities VALUES (66, 'Blaze', 'Amplifies Fire-type attacks when HP is low');
@@ -190,8 +202,6 @@ INSERT INTO abilities VALUES (87, 'Dry Skin', 'Recovers HP in rain, loses HP und
 INSERT INTO abilities VALUES (133, 'Weak Armor', 'Raises Speed and lowers Defense upon being hit');
 INSERT INTO abilities VALUES (135, 'Light Metal', 'Halves weight');
 
-
-
 INSERT INTO "poke-egg" VALUES (3,1);
 INSERT INTO "poke-egg" VALUES (3,7);
 INSERT INTO "poke-egg" VALUES (6,1);
@@ -212,7 +222,6 @@ INSERT INTO "poke-egg" VALUES (139,2);
 INSERT INTO "poke-egg" VALUES (139,9);
 INSERT INTO "poke-egg" VALUES (376,16);
 INSERT INTO "poke-egg" VALUES (635,14);
-
 
 UPDATE abilities SET IDability=97 WHERE IDability=92;
 INSERT INTO "poke-ability" VALUES (3,65,'1');
@@ -249,41 +258,9 @@ INSERT INTO "poke-ability" VALUES (376,29,'1');
 INSERT INTO "poke-ability" VALUES (376,135,'3');
 INSERT INTO "poke-ability" VALUES (635,26,'1');
 
-
-CREATE TABLE categories (
-  IDcat SMALLINT NOT NULL,
-  category CHAR(8) NOT NULL,
-   PRIMARY KEY (IDcat)
-);
-
-CREATE TABLE moves (
-  IDmove SMALLINT NOT NULL,
-  movename CHAR(16) NOT NULL ,
-  IDtype SMALLINT NOT NULL,
-  IDcat SMALLINT NOT NULL,
-  power SMALLINT NOT NULL,
-  accuracy SMALLINT NOT NULL,
-  PP SMALLINT NOT NULL,
-  effect VARCHAR(255) NOT NULL,
-  PRIMARY KEY (IDmove),
-  CONSTRAINT fk_moves_type FOREIGN KEY (IDtype) REFERENCES type (IDtype),
-  CONSTRAINT fk_moves_cat FOREIGN KEY (IDcat) REFERENCES categories (IDcat)
-);
-
-CREATE TABLE learnsets (
-	IDpoke SMALLINT NOT NULL,
-	IDmove SMALLINT NOT NULL,
-    level SMALLINT NOT NULL,
-	PRIMARY KEY (IDpoke,IDmove),
-	CONSTRAINT fk_learnset_poke FOREIGN KEY (IDpoke) REFERENCES pokemon (IDpoke),
-	CONSTRAINT fk_learnset_move FOREIGN KEY (IDmove) REFERENCES moves (IDmove)
-);
-
-
 INSERT INTO categories VALUES (1, 'Physical');
 INSERT INTO categories VALUES (2, 'Special');
 INSERT INTO categories VALUES (3, 'Status');
-
 
 INSERT INTO moves VALUES (56, 'Hydro Pump', 11, 2, 110, 80, 5, 'None');
 INSERT INTO moves VALUES (350, 'Rock Blast', 6, 1, 25, 90, 10, 'Hits 2-5 times');
